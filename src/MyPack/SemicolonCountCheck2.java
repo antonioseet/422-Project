37939bde9efbade22db3411438ee04322817cff3
Tony Barrera <antonioseet1@gmail.com>
2022-09-29 22:59:02 -0700

    Struggliging through the errors

---
+++ src/MyPack/SemicolonCountCheck2.java
@@ -0,0 +1,45 @@
+package MyPack;
+ 
+import com.puppycrawl.tools.checkstyle.api.*;
+ 
+public class SemicolonCountCheck2 extends AbstractCheck {
+ 
+    private int counter = 0;
+    
+    @Override
+    public void beginTree(DetailAST rootAST) {
+    	this.counter = 0;
+    }
+    
+    @Override
+    public void finishTree(DetailAST rootAST) {
+    	log(rootAST, CatchMsg());
+    }
+    
+    @Override
+    public int[] getDefaultTokens() {
+        return new int[] {TokenTypes.SEMI};
+    }
+ 
+    @Override
+    public void visitToken(DetailAST aAST) {
+        this.counter++;
+    }
+
+	@Override
+	public int[] getAcceptableTokens() {
+		// TODO Auto-generated method stub
+		return new int[] {TokenTypes.SEMI}; 
+	}
+
+	@Override
+	public int[] getRequiredTokens() {
+		// TODO Auto-generated method stub
+		return new int[0];
+	}
+	
+	private String CatchMsg() {
+		return "You've used a total of: " + 2 + " semicolons";
+	}
+ 
+}
