b76cedb42eefbe014ba34ccdda460f01b1880c27
Tony Barrera <antonioseet1@gmail.com>
2022-10-01 23:45:48 -0700

    Deliverable 1 complete

---
+++ Del1/BCheck.java
@@ -0,0 +1,53 @@
+package MyPack;
+ 
+import com.puppycrawl.tools.checkstyle.api.*;
+ 
+public class BCheck extends AbstractCheck {
+ 
+    private int counter = 0;
+    
+    @Override
+    public void beginTree(DetailAST rootAST) {
+    	this.counter = 0;
+    }
+    
+    @Override
+    public void finishTree(DetailAST rootAST) {
+    	log(rootAST, CatchMsg());
+    }
+    
+    @Override
+    public int[] getDefaultTokens() {
+        return new int[] {	TokenTypes.SINGLE_LINE_COMMENT,
+        					TokenTypes.BLOCK_COMMENT_BEGIN};
+    }
+ 
+    @Override
+    public void visitToken(DetailAST aAST) {
+        this.counter++;
+    }
+
+	@Override
+	public int[] getAcceptableTokens() {
+		// Auto-generated method stub
+		return new int[] {	TokenTypes.SINGLE_LINE_COMMENT,
+							TokenTypes.BLOCK_COMMENT_BEGIN}; 
+	}
+
+	@Override
+	public int[] getRequiredTokens() {
+		// Auto-generated method stub
+		return new int[0];
+	}
+	
+	@Override
+	public boolean isCommentNodesRequired() {
+		return true;
+	}
+	
+	// Returns the total number of comments in the program's
+	private String CatchMsg() {
+		return "You've used a total of: " + this.counter + " comment(s)";
+	}
+ 
+}
--- Del1/Del1-README.md
+++ Del1/Del1-README.md
@@ -1,3 +1,7 @@
 # 422-Projectn Deliverable 1
 
 This folder contains ChackA, and CheckB.
+
+ACheck contains the Halstead Length check where it checks for the total of operators and operands.
+
+BCheck contains the comment count checker, where it reports the number of comments in the file.
